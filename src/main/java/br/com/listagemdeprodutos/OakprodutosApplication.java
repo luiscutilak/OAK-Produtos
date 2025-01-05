2f7ae468e2e272b3d69d5b4aab4febcdd5a7912d
Luis Fernando Cutilak <luiscutilak@gmail.com>
2025-01-05 19:19:54 -0300

    oak produtos- first commit

---
+++ src/main/java/br/com/listagemdeprodutos/OakprodutosApplication.java
@@ -0,0 +1,13 @@
+package br.com.listagemdeprodutos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OakprodutosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OakprodutosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/listagemdeprodutos/OakprodutosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.listagemdeprodutos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OakprodutosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
