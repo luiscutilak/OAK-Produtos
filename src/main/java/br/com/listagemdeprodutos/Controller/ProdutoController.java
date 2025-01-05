791be8fa7569f895c2166381ac290a98403b56ad
Luis Fernando Cutilak <luiscutilak@gmail.com>
2025-01-05 20:22:38 -0300

    feat: base da aplicação criada

--- src/main/java/br/com/listagemdeprodutos/Controller/ProdutoController.java
+++ src/main/java/br/com/listagemdeprodutos/Controller/ProdutoController.java
@@ -1,10 +1,13 @@
 package br.com.listagemdeprodutos.Controller;
 
+import br.com.listagemdeprodutos.model.Produto;
 import br.com.listagemdeprodutos.service.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class ProdutoController {
@@ -16,5 +19,20 @@ public class ProdutoController {
     public String home(Model model) {
     model.addAttribute("produtos", produtoService.findAll());
     return "produtos";
+    }
+
+    @GetMapping("/novo")
+    public String formProduto(Model model) {
+        model.addAttribute("produto", new Produto());
+        return "formProduto";
+    }
+    @PostMapping("/salvar")
+    public String salvarProduto(@ModelAttribute Produto produto) {
+
+        if (!produto.isDisponivel()) {
+            return "redirect:/erro";
+        }
+        produtoService.save(produto);
+        return "redirect:/";
     }
 }
